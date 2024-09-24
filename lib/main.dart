253f7556c165e794bf2e439ab9dc69ae72ec8e57
JorgeNava <jorgenavadelapena@gmail.com>
2024-09-24 11:27:23 -0600

    First working

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,11 @@ import 'package:pro_directo/screens/register_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  try {
+    await Firebase.initializeApp();
+  } catch (e) {
+    print('Firebase initialization error: $e');
+  }
   runApp(const MyApp());
 }
 
@@ -18,29 +22,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       //home: LoginScreen(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      initialRoute: '/',
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //initialRoute: '/',
       routes: {
-        '/': (context) => LoginScreen(),
+        //'/': (context) => LoginScreen(),
+        '/': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
         '/register': (context) => RegisterScreen(),
         '/home': (context) => const MyHomePage(title: 'Flutter Demo Home Page'),
       },
